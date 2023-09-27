f26bcb33198971cfcfdaa79ce2ffa6900b88789f
Shubham Rai <shubham56237@gmail.com>
2023-09-28 00:04:37 +0530

    routing completed

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import React from 'react';
 import NavBar from './components/navbar/NavBar';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import ContactList from "./components/contacts/contactlist/ContactList"
+import AddContact from './components/contacts/addcontact/AddContact';
+import ViewContact from './components/contacts/viewcontact/ViewContact';
+import EditContact from './components/contacts/editcontact/EditContact';
 
 let App = () => {
   return (
     <>
       <NavBar />
       <Routes>
-        <Route path={"/"} />
-        <Route path={"/contact/list"} element={<ContactList />} />
+        <Route path={"/"} element={<Navigate to={"contacts"} />} />
+        <Route path={"/contacts/list"} element={<ContactList />} />
+        <Route path={"/contacts/add"} element={<AddContact />} />
+        <Route path={"/contacts/add"} element={<AddContact />} />
+        <Route path={"/contacts/view/:contactId"} element={<ViewContact />} />
+        <Route path={"/contacts/edit/:contactId"} element={<EditContact />} />
       </Routes>
 
     </>
