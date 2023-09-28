11d6d7e53f5cde2c63b18c62b1de30b1323763d6
Shubham Rai <shubham56237@gmail.com>
2023-09-28 18:51:46 +0530

    navbar

--- src/components/contacts/contactlist/ContactList.jsx
+++ src/components/contacts/contactlist/ContactList.jsx
@@ -4,15 +4,17 @@ import { Link } from "react-router-dom"
 let ContactList = () => {
     return (
         <>
-            <section className="contact-search">
-                <div className="container">
-                    <div className="grid">
-                        <div className="row">
-                            <div className="col">
-                                <p className="h3 m-4"><span className="text-xl font-semibold"> Contact Manager</span>
-                                    <Link to={"/contacts/add"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 m-5 px-4 rounded">
-                                   <i className="fa fa-plus-circle"/> New</Link></p>
-                            </div>
+            <section className="contact-search container">
+                <div className="">
+                    <p className=" m-4"><span className="text-xl font-semibold"> Contact Manager</span>
+                        <Link to={"/contacts/add"} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 m-5 px-4 rounded">
+                            <i className="fa fa-plus-circle" /> New</Link></p>
+                    <p className="italic font-light ">loremlorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem lorem"</p>
+                    <div className="form">
+                        <div class="mb-4">
+                           
+                            <input className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:outline-1" id="search" type="text" placeholder="Search Here" />
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 m-2 rounded">Search</button>
                         </div>
                     </div>
                 </div>
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -4,12 +4,11 @@ import { Link } from "react-router-dom"
 let NavBar = () => {
     return (
         <>
-            <nav className=" h-15 bg-slate-400">
-                <ul className="flex h-full justify-start font-bold ">
-                    <Link className="m-3 text-xl" to={"/"}>
+            <nav className=" h-15 bg-slate-400 ">
+                <ul className="flex h-full justify-start font-bold container ">
+                    <Link className=" text-xl m-4" to={"/" }>
                        <i className="fa fa-mobile text-gray-900" /> Contact  <span className="text-gray-900">Manager</span>
                     </Link>
-
                 </ul>
             </nav>
         </>
